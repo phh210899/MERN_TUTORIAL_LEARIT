422df63aa48a94452b991d4239940e438457a37d
phạm hồng hải <phamhonghai210899@gmail.com>
2021-12-18 20:22:17 +0700

    cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ const connectDB = async () => {
 connectDB()
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {origin: "*"}
+))
 app.use("/api/auth", authRouter)
 app.use("/api/posts", postRouter)
 
