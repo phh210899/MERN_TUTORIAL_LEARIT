395118265026561c348082d17c09bd74e1cf8593
phạm hồng hải <phamhonghai210899@gmail.com>
2021-12-19 18:28:39 +0700

    done

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
-    : ""
+    ? "https://sheltered-thicket-38364.herokuapp.com/api"
+    : "http://localhost:5000/api"
 
 export const LOCAl_STORAGE_TOKEN_NAME = "learnit-mern"
 
