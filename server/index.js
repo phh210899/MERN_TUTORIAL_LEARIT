f95f77b6529cf91b128932a7124fef2126ab7565
phạm hồng hải <phamhonghai210899@gmail.com>
2021-12-19 09:10:22 +0700

    better it

--- server/index.js
+++ server/index.js
@@ -23,15 +23,7 @@ const connectDB = async () => {
 connectDB()
 const app = express()
 app.use(express.json())
-app.use(
-  cors()
-)
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
+app.use(cors())
 app.use("/api/auth", authRouter)
 app.use("/api/posts", postRouter)
 
